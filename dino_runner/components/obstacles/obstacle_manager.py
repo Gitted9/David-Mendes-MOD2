6fe219ba5df5cb8dc4cd21bb8de5281d8f9f6c83
Gitted9 <m.brito.david@gmail.com>
2023-08-14 10:03:46 -0300

    MOD2/AULA3/DAVID_MENDES

---
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -0,0 +1,23 @@
+import pygame
+
+import random
+
+ 
+
+from dino_runner.components.obstacles.cactus import Cactus
+
+from dino_runner.components.obstacles.bird import Bird
+
+class ObstacleManager:
+
+    def __init__(self):
+        pass
+
+    def update(self, game):
+        pass
+        
+    def reset_obstacles(self):
+        pass
+
+    def draw(self, screen):
+        pass
